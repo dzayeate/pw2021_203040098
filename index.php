f6f2853c02909f72cecf14a58eeb4baf2f09acc1
Agam <50524722+dzayeate@users.noreply.github.com>
2021-06-04 14:09:54 +0700

    Test

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>01001000 01100101 01110010 01110110 01101001 01101110 00100000 01000110 01100001 01101011 01101000 01110010 01110101 01101100</title>
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+</head>
+<body style="background-image: url('praktikum/bg-front/bg.jpg'); background-size: cover">
+  <div class="mt-5">
+    <div class="card text-white bg-secondary m-auto" style="width: 18rem;">
+      <div class="card-header text-center">
+        <h3>MENU</h3>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><a href="kuliah" class="btn btn-primary">Kuliah</a></li>
+        <li class="list-group-item"><a href="praktikum" class="btn btn-primary">Praktikum</a></li>
+        <li class="list-group-item"><a href="tubes" class="btn btn-primary">Tubes</a></li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
